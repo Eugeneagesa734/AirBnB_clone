057166bc5db954e4b56652346958c810d6b62b06
Eugene Agesa <eugeneagesa734@gmail.com>
2024-02-11 14:40:48 -0800

    Base model from dictionary code

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,21 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+                    self.id = str(uuid.uuid4())
 
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+                    self.created_at = datetime.utcnow()
+                    self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -38,16 +48,15 @@ class BaseModel:
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
 
-    if __name__ == "__main__":
-        my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        print(my_model)
-        my_model.save()
-        print(my_model)
-        my_model_json = my_model.to_dict()
-        print(my_model_json)
-        print("JSON of my_model:")
-        for key in my_model_json.keys():
-            print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
